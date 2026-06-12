b6ee53a74492ef997244e61e1a4d2efcd3dc5bf7
Bovvbo <haibo.lin2010@gmail.com>
2026-06-12 21:02:45 +0200

    soft delete

---
+++ students.py
@@ -0,0 +1,175 @@
+from flask import Flask, render_template, request, redirect, url_for, Response
+from werkzeug.utils import secure_filename
+import sqlite3
+import os
+import uuid
+import stat
+
+app = Flask(__name__)
+
+DATABASE = "simple_crud_photo.db"
+
+
+def get_db():
+    conn = sqlite3.connect(DATABASE)
+    # conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    year_of_birth INTEGER,
+    photo BLOB,
+    photo_mime TEXT,
+    photo_filename TEXT,
+    photo_path TEXT,
+    deleted_at TIMESTAMP NULL DEFAULT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+    """)
+    conn.commit()
+    conn.close()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/students")
+def students():
+    cnx = get_db()
+    cursor = cnx.cursor()
+
+    query = "select * from students WHERE deleted_at IS NULL;"
+    cursor.execute(query)
+    students = cursor.fetchall()
+
+    cursor.close()
+    cnx.close()
+    return render_template("./students.html", students=students)
+
+
+@app.route("/student_add")
+def student_add(name=None):
+    return render_template("student_add.html", name=name)
+
+
+@app.route("/student_add_submit", methods=["post"])
+def student_add_submit():
+    cnx = get_db()
+    cursor = cnx.cursor()
+
+    photo = request.files.get("photo")
+    photo_path = None
+    if photo and photo.filename:
+        #
+        ext = os.path.splitext(secure_filename(photo.filename))[1]
+        #
+        new_filename = f"{uuid.uuid4().hex}{ext}"
+        #
+        save_dir = os.path.join(app.root_path, "static", "uploads")
+        os.makedirs(save_dir, exist_ok=True)
+        full_path = os.path.join(save_dir, new_filename)
+        photo.save(full_path)
+        #
+        os.chmod(full_path, stat.S_IRUSR | stat.S_IWUSR)
+        photo_path = f"uploads/{new_filename}"
+    # use %s not ? if you use mysql
+    insert_stmt = "insert into students(name, photo_path) " "values(?, ?)"
+    data = (request.form["name"], photo_path)
+    cursor.execute(insert_stmt, data)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    return redirect(url_for("students"))
+
+
+@app.route("/student_upd")
+def students_upd(name=None):
+    cnx = get_db()
+    cursor = cnx.cursor()
+
+    query = "select * from students where id=?"
+    cursor.execute(query, (request.args.get("id"),))
+
+    student = cursor.fetchall()[0]
+
+    cursor.close()
+    cnx.close()
+    return render_template("student_upd.html", id=student[0], name=student[1])
+
+
+@app.route("/submit_student_upd", methods=["post"])
+def submit_student_upd():
+    cnx = get_db()
+    cursor = cnx.cursor()
+
+    photo = request.files.get("photo")
+
+    photo_path = None
+    if photo and photo.filename:
+        # splittext: split extension from filename
+        ext = os.path.splitext(secure_filename(photo.filename))[1]
+        new_filename = f"{uuid.uuid4().hex}{ext}"
+        save_dir = os.path.join(app.root_path, "static", "uploads")
+        os.makedirs(save_dir, exist_ok=True)
+        full_path = os.path.join(save_dir, new_filename)
+        photo.save(full_path)
+        os.chmod(full_path, stat.S_IRUSR | stat.S_IWUSR)
+        photo_path = f"uploads/{new_filename}"
+
+        upd_stmt = "update students " "set name=?, photo_path=? " "where id=?"
+        data = (request.form["name"], photo_path, request.form["id"])
+    else:
+        upd_stmt = "update students " "SET name=? " "where id=?"
+        data = (request.form["name"], request.form["id"])
+        print(data)
+    cursor.execute(upd_stmt, data)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    return redirect(url_for("students"))
+
+
+@app.route("/student_del")
+def students_del(name=None):
+    cnx = get_db()
+    cursor = cnx.cursor()
+
+    student_id = request.args.get("id")
+
+    # 先查出这个学生的图片路径
+    query = "select photo_path from students where id=?"
+    cursor.execute(query, (student_id,))
+    row = cursor.fetchone()
+
+    if row and row[0]:
+        photo_path = row[0]
+        full_path = os.path.join(app.root_path, "static", photo_path)
+
+        if os.path.exists(full_path):
+            os.remove(full_path)
+
+    # 删除数据库记录
+    #query = "DELETE FROM students WHERE id = ?"
+    query = """
+        UPDATE students SET deleted_at = CURRENT_TIMESTAMP WHERE id=?
+        """
+    cursor.execute(query, (student_id,))
+    cnx.commit()
+
+    cursor.close()
+    cnx.close()
+    return redirect(url_for("students"))
+
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
